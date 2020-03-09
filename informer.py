79b6fad4f5dd7df9aad0d78ea86b03293023c7dc
Arnab771 <arnabchakraborty771@gmail.com>
2020-03-09 15:33:15 +0530

    Added Banner

--- informer.py
+++ informer.py
@@ -1,5 +1,9 @@
+#!/usr/bin/python3
 from selenium import webdriver
 import time
+from colorama import init, Fore
+import pyfiglet
+from config import audio
 import datetime
 import os
 import pickle
@@ -7,10 +11,6 @@ import sqlite3
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 from pygame import mixer
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
-
-# def save(data, name):
-#     with open(name, 'a') as f:
-#         f.write(data + "\n")
 
 def save(name, status, duration):
     conn = sqlite3.connect('data.db')
@@ -22,37 +22,46 @@ def save(name, status, duration):
     conn.close()
 
 def main():
-    print("!<--------------Press Ctrl+C to quit/terminate----------------->")
-    print('Enter the name of the person to track')
-    name = input('>').lower()
-    browser = webdriver.Firefox() # Use webdriver.Chrome() if using google chrome or chromium-based browsers
-    browser.get('https://web.whatsapp.com')
-    time.sleep(14)
-    flag = 0
-    temp1 = None
-    while True:
-        try:
-            elem = browser.find_element_by_class_name('_315-i')
-            if elem.text == 'online' and flag != 1:
-                mixer.init()
-                mixer.music.load('C:/Users/Arnab/Music/cycle.mp3') #Path to your audio of choice
-                mixer.music.play() 
-                status = str(datetime.datetime.now().strftime('%B %d, %Y -- %r')) + ' : online'
-                temp1 = datetime.datetime.now()
-                flag = 1
-                print(status)
-                save(name, "online", None)
-                
-        except (NoSuchElementException, StaleElementReferenceException):
-            if flag != 2:
-                status = str(datetime.datetime.now().strftime('%B %d, %Y -- %r')) + ' : offline'
-                temp2 = datetime.datetime.now()
-                if temp1 != None:
-                    duration = temp2 - temp1
-                    print("Duration: ", duration.total_seconds()//60, "mins", duration.total_seconds()%60, "secs")
-                    save(name, "offline", str(duration))
-                flag = 2
-                print(status)
+    try:
+        print("!<--------------Press Ctrl+C to quit/terminate----------------->")
+        ascii_banner = pyfiglet.figlet_format('INFORMER')
+        print(Fore.CYAN + ascii_banner)
+        print('By Arnab' + Fore.RESET)
+        print()
+        print()
+        print('Enter the name of the person to track')
+        name = input('>').lower()
+        browser = webdriver.Firefox() # Use webdriver.Chrome() if using google chrome or chromium-based browsers
+        browser.get('https://web.whatsapp.com')
+        time.sleep(14)
+        flag = 0
+        temp1 = None
+        while True:
+            try:
+                elem = browser.find_element_by_class_name('_315-i')
+                if elem.text == 'online' and flag != 1:
+                    mixer.init()
+                    mixer.music.load(audio) #Path to your audio of choice
+                    mixer.music.play() 
+                    status = str(datetime.datetime.now().strftime('%B %d, %Y -- %r')) + ' : online'
+                    temp1 = datetime.datetime.now()
+                    flag = 1
+                    print(Fore.GREEN + status)
+                    save(name, "online", None)
+                    
+            except (NoSuchElementException, StaleElementReferenceException):
+                if flag != 2:
+                    status = str(datetime.datetime.now().strftime('%B %d, %Y -- %r')) + ' : offline'
+                    temp2 = datetime.datetime.now()
+                    if temp1 != None:
+                        duration = temp2 - temp1
+                        print(Fore.YELLOW + "Duration: ", duration.total_seconds()//60, "mins", duration.total_seconds()%60, "secs")
+                        save(name, "offline", str(duration))
+                    flag = 2
+                    print(Fore.RED + status)
+    except KeyboardInterrupt:
+        print()
+        print(Fore.RED + 'Exiting......')
 
 if __name__ == "__main__":
     main()
